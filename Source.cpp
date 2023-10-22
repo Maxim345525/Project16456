0ea194324971ce5402bf05328144957a751e8d38
Maxim345525 <maximmaksimchyk@gmail.com>
2023-10-22 13:04:45 +0300

    add code

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,13 @@
-sdffgd
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout << "Input a->";
+	cin >> a;
+	int dobytok = 0;
+	for (int i = a; i <= 20; i++) {
+		dobytok += i;
+	}
+	cout << dobytok << endl;
+}
