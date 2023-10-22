081ccc9f6a3b6626bd7c6c4667280d3078584e4f
Maxim345525 <maximmaksimchyk@gmail.com>
2023-10-22 12:45:14 +0300

    add code

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,13 @@
-sdffgd
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout << "Input x->";
+	cin >> x;
+	int y;
+	cout << "Input y->";
+	cin >> y;
+	double result = pow(x, y);
+	cout << result << endl;
+}
