b713af8f14519bc777bf22ace8e41ed27bdde036
Maxim345525 <maximmaksimchyk@gmail.com>
2023-10-22 14:34:25 +0300

    add

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,16 @@
-sdffgd
+#include <iostream>
+
+int main() {
+    int k;
+    std::cout << "Input k->";
+    std::cin >> k;
+
+    std::cout << "Table of multiplication by" << k << ":" << std::endl;
+
+    for (int i = 1; i <= 10; i++) {
+        int result = k * i;
+        std::cout << k << " x " << i << " = " << result << std::endl;
+    }
+
+    return 0;
+}
