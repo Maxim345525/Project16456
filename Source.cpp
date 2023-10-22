1f16fefd459abdae45720aff3c46f8964f6e156b
Maxim345525 <maximmaksimchyk@gmail.com>
2023-10-22 12:39:11 +0300

    add code

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,21 @@
-sdffgd
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Input a: ";
+    cin >> a;
+
+    if (a < 1 || a > 500) {
+        cout << "a must be between 1 and 500." << endl;
+        return 1; 
+    }
+
+    int sum = 0;
+    for (int i = a; i <= 500; i++) {
+        sum += i;
+    }
+
+    cout << "Summa " << a << " to 500: " << sum << endl;
+    return 0;
+}
