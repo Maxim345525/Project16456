e7d44dc0ee84b8af33e4d80814eb8810b899aeee
Maxim345525 <maximmaksimchyk@gmail.com>
2023-10-22 12:50:49 +0300

    add code

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,13 @@
-sdffgd
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum = 0;
+    int n = 1000;  
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    double average = static_cast<double>(sum) / n;
+    cout << "Arithmetic average of numbers from 1 to 1000: " << average << endl;
+    return 0;
+}
